25b2f3ab2ee2a53b26e785ed501a152f585ace2a
Mayh6m <70498107+Mayh6m@users.noreply.github.com>
2020-11-12 00:15:59 -0300

    Página primo finalizada

--- src/routes/appRoutes/primo.js
+++ src/routes/appRoutes/primo.js
@@ -2,7 +2,13 @@ import express from 'express';
 import primo from '../../utils/primo';
 const route = express.Router();
 
+route.post('/primo', (req, res) => {
+  const { entrada } = req.body;
+  const resultado = primo(entrada) ? "é primo" : "não é primo";
+  res.render('pages/primo',{resultado, entrada});
+});
+
 route.get('/primo', (req, res) => {
-  res.render('pages/primo', {});
+  res.render('pages/primo', { resultado: null, entrada:null });
 });
 export default route;
