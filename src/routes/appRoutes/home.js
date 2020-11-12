c71d42e4d8da04fe807bf1d5c149d5eaf74cfc69
jacksrm <62717829+jacksrm@users.noreply.github.com>
2020-11-12 11:39:53 -0300

    consertado uns bugs

--- src/routes/appRoutes/home.js
+++ src/routes/appRoutes/home.js
@@ -3,6 +3,14 @@ import express from 'express';
 const route = express.Router();
 
 route.get('/', (req, res) => {
-  res.render('pages/home');
+  res.render('pages/home', {
+    equipe: [
+      'Jacson Rodrigies',
+      'Matheus Rodrigues',
+      'Dante Dantas',
+      'Rafael Muleque',
+      'João Monteiro'
+    ]
+  });
 });
 export default route;
