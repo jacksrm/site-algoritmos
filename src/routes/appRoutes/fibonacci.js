b7df757173d57d28a15f960553cefdb527a5befa
RafaelD3v <rafaelcontato66738621+RafaelD3v@users.noreply.github.com>
2020-11-11 21:36:12 -0300

    Fibonacci!

--- src/routes/appRoutes/fibonacci.js
+++ src/routes/appRoutes/fibonacci.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import fibonacci from '../../utils/fibonacci';
 
 const route = express.Router();
 
 route.get('/fibonacci', (req, res) => {
-  res.render('pages/fibonacci', { quantidade: '' });
+  res.render('pages/fibonacci', { resultado: '' });
+});
+
+route.post('/fibonacci', (req, res) => {
+  const { quantidade } = req.body;
+
+  const resultado = fibonacci(parseInt(quantidade));
+
+  return res.render('pages/fibonacci', { resultado });
 });
 
 export default route;
