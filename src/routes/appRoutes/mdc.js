e5369f933bbe1eb8f1d2e30502eeea0f3c6dca13
Dantedod <70494467+Dantedod@users.noreply.github.com>
2020-11-12 11:44:04 -0300

    Mdcfeito

--- src/routes/appRoutes/mdc.js
+++ src/routes/appRoutes/mdc.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import mdc from '../../utils/mdc';
 
 const route = express.Router();
 
 route.get('/mdc', (req, res) => {
-  res.render('pages/mdc', {});
+  res.render('pages/mdc', { resultado: null });
+});
+
+route.post('/mdc', (req, res) => {
+  const { numero1, numero2 } = req.body;
+
+  const resultado = mdc(numero1, numero2);
+
+  return res.render('pages/mdc', { resultado });
 });
 
 export default route;
