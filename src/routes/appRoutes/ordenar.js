d79f3749ea2a5e1f65d4f8c35b635e222f945664
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2020-11-15 11:47:03 -0300

    Implementação ejs para a ordenção, implementação do algoritmo quickSort

--- src/routes/appRoutes/ordenar.js
+++ src/routes/appRoutes/ordenar.js
@@ -1,8 +1,19 @@
 import express from 'express';
 
+import quick from '../../utils/ordenar';
+
 const route = express.Router();
 
 route.get('/ordenar', (req, res) => {
-  res.render('pages/ordenar', { header: true });
+  res.render('pages/ordenar', { resultado: null, header: true });
 });
+
+route.post('/ordenar', (req, res) => {
+  const { vetor } = req.body;
+
+  const resultado = quick(vetor);
+  
+  return res.render('pages/ordernar', { resultado, header: true });
+});
+
 export default route;
---
+++ src/utils/ordenar.js
@@ -0,0 +1,40 @@
+var left = 0;
+var right = 100;
+
+function quick(vetor, left, right){
+   var pivo = left;
+   if(left < right){
+       var pivoIndex = quebra(vetor, left, right, pivo);
+
+       quick (vetor, left, pivoIndex - 1);
+       quick (vetor, pivoIndex + 1, right);
+   }
+   return vetor;
+}
+
+function quebra(vetor, left, right, pivo){
+    var p = vetor[pivo];
+    troca (vetor, right, pivo);
+    var aux = left;
+
+    for (let i = left; i < right; i++){
+        if (vetor[i] <= p){
+            troca(vetor, i, aux);
+            aux = aux + 1;
+        }
+    }
+
+    troca(vetor, right, aux);
+    return aux;
+}
+
+function troca(vetor, i, j){
+    let temp = vetor[i];
+    vetor[i] = vetor[j];
+    vetor[j] = temp; 
+}
+
+export default quick;
+
+
+//vetor de exemplo para teste: 1, 100, 39, 200, 100, 25, 0, 45
