c5d895cc732857b7c87ab804a59c79bf05bdf5d3
jacksrm <62717829+jacksrm@users.noreply.github.com>
2020-12-14 15:08:29 -0300

    algumas modificações

--- tests/ordenar.test.js
+++ tests/ordenar.test.js
@@ -1,16 +1,16 @@
 import quick from '../src/utils/ordenar';
 
 describe('Ordenar', () => {
-    it ('Esperado ordenar um array', () =>{
-        const array = [4,5,1,0,1];
-        const ordenado = quick(array);
+  it('Esperado ordenar um array', () => {
+    const array = [4, 5, 1, 0, 1];
+    const ordenado = quick(array);
 
-        expect(ordenado).toEqual(expect.arrayContaining([0,1,1,4,5]));
-    });
+    expect(ordenado).toEqual(expect.arrayContaining([0, 1, 1, 4, 5]));
+  });
 
-    it ('Esperado um array ordenado com sete posições', () => {
-        const array = [4,5,1,0,1, , ,];
-        const ordenado = quick(array);
-        expect(ordenado).toHaveLength(7);
-    })    
+  it('Esperado um array ordenado com sete posições', () => {
+    const array = [4, 5, 1, 0, 1, , ,];
+    const ordenado = quick(array);
+    expect(ordenado).toHaveLength(7);
+  });
 });
