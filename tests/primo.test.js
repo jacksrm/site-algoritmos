9b3e13979390db8a81e7579550bf7085a309f764
jacksrm <62717829+jacksrm@users.noreply.github.com>
2020-12-14 18:48:12 -0300

    algumas modificações

--- tests/primo.test.js
+++ tests/primo.test.js
@@ -1,18 +1,17 @@
-
 import primo from '../src/utils/primo';
 
 describe('Retorna se o número digitado é pertencente a família de numeros primos', () => {
-    it('Almeja-se que retorne um valor falso', () => {
-        const num = 16;
-        const isPrimo = primo(num);
+  it('Almeja-se que retorne um valor falso', () => {
+    const num = 16;
+    const isPrimo = primo(num);
 
-        expect(isPrimo).toBeFalsy();
-    });
+    expect(isPrimo).toBeFalsy();
+  });
 
-    it('Almeja-se que retorne um valor verdadeiro', () => {
-        const num = 2;
-        const isPrimo = primo(num);
+  it('Almeja-se que retorne um valor verdadeiro', () => {
+    const num = 2;
+    const isPrimo = primo(num);
 
-        expect(isPrimo).toBeTruthy();
-    });
+    expect(isPrimo).toBeTruthy();
+  });
 });
