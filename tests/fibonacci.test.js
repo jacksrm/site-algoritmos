bc509ce8cd9b8385b5af0e60c4514685120b93e0
RafaelD3v <rafaelcontato66738621+RafaelD3v@users.noreply.github.com>
2020-12-14 14:43:03 -0300

    Fibonacci-OK!

---
+++ tests/fibonacci.test.js
@@ -0,0 +1,17 @@
+import fibonacci from '../src/utils/fibonacci';
+
+describe('Mostrar a saida da sequencia de fibonacci', () => {
+    it('Esperado mostrar a sequencia de fibonacci', () => {
+        const quantidade = 5;
+        const sequencia = fibonacci(quantidade);
+
+        expect(sequencia).toBe('0 1 1 2 3 ');
+    });
+
+    it('Esperado falha ao mostrar sequencia', () => {
+        const quantidade = 's';
+        const sequencia = fibonacci(quantidade);
+
+        expect(sequencia).toBe('');
+    });
+});
