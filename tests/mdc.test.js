8a6c77490db2cb2a1637ee570e6086f66a683a3b
Dantedod <70494467+Dantedod@users.noreply.github.com>
2020-12-14 14:38:54 -0300

    Teste MDC

---
+++ tests/mdc.test.js
@@ -0,0 +1,20 @@
+import mdc from '../src/utils/mdc';
+
+describe('Fazer MDC dos números', () => {
+  it('Esperado fazer o MDC dos números escolhidos', () => {
+    const Num1 = 10;
+    const Num2 = 12;
+    const result = mdc(Num1, Num2);
+
+    expect(result).toBe(2);
+  });
+  describe('fazer MDC dos números', () => {
+    it('Esperado fazer o MDC dos números escolhidos', () => {
+      const Num1 = 'sdadsadsa';
+      const Num2 = 'sdadsada';
+      const result = mdc(Num1, Num2);
+
+      expect(result).toBe(undefined);
+    });
+  });
+});
